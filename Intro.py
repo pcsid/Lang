324bc44840e267da0055a8bb757cd28b39ea2d83
pcsid <40740959+pcsid@users.noreply.github.com>
2025-02-10 10:55:21 -0600

    initial

---
+++ Intro.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+response = model.invoke("Hello, world!")
+print(response)
