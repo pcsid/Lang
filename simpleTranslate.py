5e7353dbdd6e7aac02d870461e860ff12d215b6a
pcsid <40740959+pcsid@users.noreply.github.com>
2025-02-11 14:53:22 -0600

    simple Translator

--- simpleTranslate.py
+++ simpleTranslate.py
@@ -1,10 +1,25 @@
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
-
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-response = model.invoke("Hello, world!")
-print(response)
+'''
+#Part 1 - manual prompt
+input_text = input("Enter the text to translate: ")
+
+messages = [
+    SystemMessage(content="Your persona is an expert English to French translator"),
+    SystemMessage(content="Your job is to translate the text from English to French - no other text is needed"),
+    HumanMessage(content=input_text)
+]
+
+#response = model.invoke(messages)
+#print(response)
+#part 2 - streaming
+for token in model.stream(messages):
+    print(token.content, end="", flush=True)
+'''
